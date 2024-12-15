e18009c784ce60c8c6431f4904d567a7d7746397
fsvdavi <d.silva.viana.red@gmail.com>
2024-12-15 14:21:56 -0300

    Adicao de construtores a classe lampada

--- src/br/com/cursoPOO/lampada/Lampada.java
+++ src/br/com/cursoPOO/lampada/Lampada.java
@@ -22,4 +22,30 @@ public class Lampada {
 	    System.out.println("O preço foi alterado para: " + novoPreco);
 	    
 	}
+	
+	//CONSTRUTORES
+	
+	Lampada() {
+		System.out.println("A classe lâmpada foi instanciada!");
+	}
+	
+	Lampada(String tipo, String formato) {
+		this.tipo = tipo;
+		this.formato = formato;
+		System.out.println("A lâmpada é" + this.tipo + " e tem fomato de " + this.formato);		
+	}
+	
+	Lampada(boolean ligada, double preco, double potencia) {
+		this.ligada = ligada;
+		this.preco = preco;
+		this.potencia = potencia;
+			if (this.ligada) {
+				System.out.println("A lâmpada está acesa");				
+			} else {
+				System.out.println("A lâmpada está apagada");
+			}
+		System.out.println("A lâmpada custa " + this.preco + " R$ e possui uma potência de " + 
+		this.potencia + this.potencia);
+		
+	}
 }
